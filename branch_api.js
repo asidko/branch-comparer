bf75f83b48a3fd229eeaef026c885db1cc344a2b
Alex Sidko <sidkoo@outlook.com>
2023-05-28 12:13:54 +0300

    Add merge branches feature

--- branch_api.js
+++ branch_api.js
@@ -22,7 +22,16 @@ router.get('/compare', async (req, res) => {
     const branchCompareResult = await githubApi.compareBranches(repoOwner, repoName, branchName, baseBranchName);
     res.json(branchCompareResult);
 });
-
+router.get('/merge', async (req, res) => {
+    console.log("REST. GET /api/merge. Merging branches for repository: " + req.query.url + " branch: " + req.query.branch + " baseBranch: " + req.query.baseBranch)
+    validateRequiredParams(req, res, ['url', 'branch', 'baseBranch'])
+    const repoUrl = req.query.url;
+    const branchName = req.query.branch;
+    const baseBranchName = req.query.baseBranch;
+    const {repoOwner, repoName} = getOwnerAndRepoFromUrl(repoUrl);
+    const mergeResult = await githubApi.mergeBranches(repoOwner, repoName, branchName, baseBranchName);
+    res.json(mergeResult);
+});
 function validateRequiredParams(req, res, requiredParamNames) {
     const missingParams = [];
     requiredParamNames.forEach(paramName => {
